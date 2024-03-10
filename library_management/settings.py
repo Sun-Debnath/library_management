198edb51f6b8298a776833f384da7f2a665983a9
Sun-Debnath <sundebnath2300@gmail.com>
2024-03-10 19:52:01 +0600

    setting

--- library_management/settings.py
+++ library_management/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://library-management-u7k9.onrender.com', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://randomize-event-solution.onrender.com/', 'https://*.127.0.0.1']
 
 
 # Application definition
